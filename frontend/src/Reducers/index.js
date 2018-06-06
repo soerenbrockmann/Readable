5380e24f70df2a58e63f04e031cb6a76adfcf1d5
Sören Brockmann <>
2018-06-06 09:57:59 +0200

    Deleted old reducers folder structure

--- frontend/src/Reducers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as categoriesReducer } from "./categoriesReducer";
-export { default as postsReducer } from "./postsReducer";
-export { default as sortReducer } from "./sortReducer";
-export { default as loginReducer } from "./loginReducer";
-export { default as commentsReducer } from "./commentsReducer";
